f6ff22d55f71d61ca117aa9d97599e44c4c8bac1
Andrey Shein <spallhex@mail.ru>
2023-09-02 03:38:19 +0700

    -Added number of attempts -Fixed some bugs

--- Program.cs
+++ Program.cs
@@ -17,38 +17,48 @@ namespace GuessTheNumber
         }
         static void Main(string[] args)
         {
-            string userAnswer;
-            string userResponse;
-            int userNum = 0;
+            string userResponse; //user choice on whether to restart the game
+            int userNum = 0; //user number
             bool isPlayAgain = true;
+            int compNum; //computer guessed number
 
             while (isPlayAgain)
             {
+                int attempts = 5; //number of attempts (ENTER THE NUMBER OF ATTEMPTS!)
                 Console.WriteLine("Welcome to Guess the Number!\nI guessed a number from 1 to 100! Can you guess what I guessed?");
-                int compNum = NumberGenerator();
+                compNum = NumberGenerator();
 
                 while (userNum != compNum)
                 {
-                    Console.Write("Write a number: ");
-                    userAnswer = Console.ReadLine();
-                    userNum = int.Parse(userAnswer);
+                    Console.Write($"Write a number (You only have {attempts} attempts!): ");
+                    userNum = Convert.ToInt32(Console.ReadLine());
 
                     if (userNum < 1 || userNum > 100)
                     {
-                        Console.WriteLine("Wrong number! Enter number again!");
+                        Console.WriteLine("Wrong number and character that is not a number!");
+                        break;
                     }
 
                     if (userNum > compNum)
                     {
                         Console.WriteLine($"{userNum} is to high!");
+                        --attempts;
                     }
                     else if (userNum < compNum)
                     {
                         Console.WriteLine($"{userNum} is to low!");
+                        --attempts;
                     }
                     else
                     {
-                        Console.WriteLine($"You won! I guessed {compNum}");
+                        Console.WriteLine($"YOU WIN! I guessed {compNum}");
+                        break;
+                    }
+
+                    if (attempts <= 0)
+                    {
+                        Console.WriteLine("YOU LOSE! The attempts are over!");
+                        break;
                     }
 
                 }
